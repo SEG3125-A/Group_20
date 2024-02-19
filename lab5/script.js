725639eb98a9dc59f8216506ed9e9e394f0eccfe
rinaxosman <94803092+rinaxosman@users.noreply.github.com>
2024-02-18 20:37:02 -0500

    Lab 5 fixes

--- lab5/script.js
+++ lab5/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    // Smooth scrolling for navbar links
     $('a.nav-link').click(function(e) {
         e.preventDefault();
         var target = $(this).attr('href');
@@ -8,16 +7,13 @@ $(document).ready(function() {
         }, 1000);
     });
 
-    // Form submission placeholder
     $('form').submit(function(e) {
-        e.preventDefault(); // Prevent actual form submission
+        e.preventDefault(); // Prevents
         alert('Booking submitted!');
     });
 
-    // Example of input validation (for demonstration purposes)
     $('#cardInfo').on('input', function() {
         var cardNumber = $(this).val();
-        // Simple validation for demo: check if the input length is 16 digits
         if(cardNumber.length === 16) {
             $(this).css('border-color', 'green');
         } else {
