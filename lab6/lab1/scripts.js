19c7eb09d5d6675578bc7d1b0e89f8ce0470e71f
rinaxosman <94803092+rinaxosman@users.noreply.github.com>
2024-02-22 15:50:30 -0500

    ui updates

--- lab6/lab1/scripts.js
+++ lab6/lab1/scripts.js
@@ -19,3 +19,44 @@ function showNoMessage() {
   openToast();
 }
 
+function scrollToTop() {
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+  });
+}
+function showInstructions() {
+  var instructionsText = document.getElementById('instructionsText');
+  instructionsText.style.display = 'block';
+}
+
+function closeInstructions() {
+  var instructionsText = document.getElementById('instructionsText');
+  instructionsText.style.display = 'none';
+}
+
+function openScreenshot() {
+  window.open('YT-homepage.png', 'YouTube Homepage Screenshot', 'width=1536,height=864');
+}
+
+function scrollToQuestion() {
+  var question1Top = document.getElementById('q1').offsetTop;
+  window.scrollTo({
+      top: question1Top,
+      behavior: 'smooth'
+  });
+}
+
+function showNoMessage() {
+  var noMessageText = document.getElementById('noMessageText');
+  noMessageText.style.display = 'block';
+}
+
+function closeToast() {
+  var noMessageText = document.getElementById('noMessageText');
+  noMessageText.style.display = 'none';
+}
+
+function submitResponses() {
+  alert("Responses submitted successfully!");
+}
