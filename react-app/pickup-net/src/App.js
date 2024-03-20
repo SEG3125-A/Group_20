ac836390b4b31f6f518c1f3f7e508702619dec5b
rinaxosman <94803092+rinaxosman@users.noreply.github.com>
2024-03-20 17:47:03 -0400

    test

--- react-app/pickup-net/src/App.js
+++ react-app/pickup-net/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/Navbar/navbar.css';
 import './App.css';
 import Navbar from './components/Navbar/navbar.js';
-import Title from './title.js';
-import Image from './mainimage.js';
 import HelpIcon from './components/HelpIcon/helpIcon.js';
 import Sports from './components/Navbar/sports.js';
 import Leagues from './components/Navbar/leagues.js';
 import MasterSchedule from './components/Navbar/masterSchedule.js';
-import ActionButton from './ActionButton.js';
 import Home from './Home.js';
 
 
