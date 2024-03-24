311e03bcaa9fc36158e12fe0c14f2941722dc0dc
rinaxosman <94803092+rinaxosman@users.noreply.github.com>
2024-03-24 17:35:50 -0400

    cart-forum titles

--- react-app/pickup-net/src/components/Navbar/cart.js
+++ react-app/pickup-net/src/components/Navbar/cart.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import '../../App.css';
 
 function Cart({ cart }) {
     return (
-        <div className="cart">
-            <h2>Shopping Cart</h2>
-            {/* {cart.length === 0 ? (
-                <p>Your cart is empty.</p>
-            ) : (
-                <ul>
-                    {cart.map((item, index) => (
-                        <li key={index}>{item.title}</li>
-                    ))}
-                </ul>
-            )} */}
+        <div className='page'>
+            <div className='cart'>
+                <h2>Shopping Cart</h2>
+                {/* {cart.length === 0 ? (
+                    <p>Your cart is empty.</p>
+                ) : (
+                    <ul>
+                        {cart.map((item, index) => (
+                            <li key={index}>{item.title}</li>
+                        ))}
+                    </ul>
+                )} */}
+            </div>
         </div>
     );
 }
--- react-app/pickup-net/src/components/Navbar/forum.js
+++ react-app/pickup-net/src/components/Navbar/forum.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import '../../App.css';
-import ForumTitle from './ForumTitle.js';
-import NotificationForm from './NotificationForm'; 
-import './notificationform.css';
-import ActionButton from '../../ActionButton.js';
 
 function Forum() {
-  return (
-    <div className="dropin-content">
-      <ForumTitle/>
-      <NotificationForm />
-    </div>
-  );
+    return (
+        <div className='page'>
+          <h2>Discussion Board</h2>
+        </div>
+    );
 }
 
 export default Forum;
